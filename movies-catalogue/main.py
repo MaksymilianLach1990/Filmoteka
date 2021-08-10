c9217b100731988d50f47083a030ffb2b9ca5408
Krzysztof Lach <lachkrzysztof56@gmail.com>
2021-08-10 20:09:33 +0200

    Add contact

--- movies-catalogue/main.py
+++ movies-catalogue/main.py
@@ -7,8 +7,13 @@ app.config["ENV"] = "development"
 
 @app.route('/')
 def homepage():
-    return render_template("homepage.html")
+    movies = ["Spider-man", "Batman", "Red", "Superman", "Transformens", "Drakula"]
+    return render_template("homepage.html", movies=movies)
 
+
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    return render_template("contact.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
